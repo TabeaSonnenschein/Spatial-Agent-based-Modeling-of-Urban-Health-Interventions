de5a3ffb1869c9cc4e40088f1477a5c8ca55ff3e
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2021-06-18 19:52:36 +0200

    Update Spatial Joins.R

--- Spatial Joins.R
+++ Spatial Joins.R
@@ -3,28 +3,36 @@ pkgs <- c("sf", "sp", "stplanr", "rgdal", "spatialEco")
 sapply(pkgs, require, character.only = T) #load 
 rm(pkgs)
 
-crs <- "+init=EPSG:23095" #ED_1950_TM_5_NE
-crs <- "+init=EPSG:28992" #Amersfoort / RD New
-
 
 dsn_data = setwd("C:/Dokumente/PhD EXPANSE/Data/Amsterdam/Administrative Units")
-polygon = readOGR(dsn=dsn_data ,layer="Netherlands_Neighborhoods", driver="ESRI Shapefile")
-colnames(polygon@data)
-polygon@data = polygon@data[,c("buurtcode" , "buurtnaam")]
-polygon = spTransform(polygon, CRSobj = crs)
-proj4string(polygon)=CRS(crs) 
+polygon = readOGR(dsn=dsn_data ,layer="Netherlands_Neighborhoods")
+colnames(polygon@data) # select only columns that should be joined to point dataset
 
 dsn_data = setwd("C:/Dokumente/PhD EXPANSE/Data/Amsterdam/Built Environment/Buildings")
-point = readOGR(dsn=dsn_data ,layer="Residences", driver="ESRI Shapefile")
-residences = spTransform(point, CRSobj = crs)
-proj4string(point)=CRS(crs) 
-
-## needs to be same coordinate reference system!
-polygon@proj4string
-point@proj4string
-
-point <- point.in.poly(point, polygon, sp = T)
-colnames(point@data) #check if the desired columns are present
+point = readOGR(dsn=dsn_data ,layer="Residences")
 
 
-plot(point)
+
+joinPolygonAttr_to_PointSPDF = function(polygon_SPDF, point_SPDF, Country_ISO, poly_col_list){
+  if(Country_ISO == "NL"){
+    crs = "+init=EPSG:28992" #Amersfoort / RD New
+  }
+  polygon_SPDF@data = polygon_SPDF@data[,poly_col_list] # select only columns that should be joined to point dataset
+  polygon_SPDF = spTransform(polygon_SPDF, CRSobj = crs)
+  proj4string(polygon_SPDF)=CRS(crs) 
+  point_SPDF = spTransform(point_SPDF, CRSobj = crs)
+  proj4string(point_SPDF)=CRS(crs) 
+  if(as.character(polygon_SPDF@proj4string) == as.character(point_SPDF@proj4string)){ ## needs to be same coordinate reference system!
+    newSdf <- point.in.poly(point_SPDF, polygon_SPDF, sp = T)
+    print(paste("New columns of Spatial Point Dataframe:", paste(colnames(newSdf@data))))  #check if the desired columns are present
+    return(newSdf) 
+   }
+  else{
+    print("Not possible to transform spatial datasets into same CRS")
+  }
+}
+
+point = joinPolygonAttr_to_PointSPDF(polygon_SPDF = polygon, point_SPDF = point, Country_ISO = "NL", poly_col_list= c("buurtcode" , "buurtnaam"))
+
+dsn_data = setwd("C:/Dokumente/PhD EXPANSE/Data/Amsterdam/Built Environment/Buildings")
+writeOGR(point, dsn=dsn_data ,layer="Residences_neighcode_RDNew",driver="ESRI Shapefile")
