d6c54363fe2e41e298d1bf1e01bdc097f12cbf39
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2021-09-28 17:31:41 +0200

    c

---
+++ Walkability indexes.R
@@ -0,0 +1,30 @@
+##############################
+##Walkability index###########
+##############################
+#criteria
+
+#1 Population density
+#2 Density of retail and service destinations (retail environment)
+#3 Land-use mix
+#4 Street connectivity (intersection density)
+#5 Green space
+#6 Side walk presence
+
+
+
+
+
+#optional criteria
+
+# Safety from crime
+# Traffic safety
+# Traffic volume and speed
+# Pedestrian crossing availability
+# Aesthetics
+# Air quality
+# Shade or sun in appropriate seasons
+# Street furniture
+# Wind conditions
+# Specific walking destinations such as light rail stops and bus stops (Brown et al., 2009)
+# Job density
+
