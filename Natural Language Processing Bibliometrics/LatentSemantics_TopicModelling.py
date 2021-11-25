5172f1df5ccf40b6c3e511d4f4936f9649756999
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2021-11-25 11:10:32 +0100

    Update LatentSemantics_TopicModelling.py

--- Natural Language Processing Bibliometrics/LatentSemantics_TopicModelling.py
+++ Natural Language Processing Bibliometrics/LatentSemantics_TopicModelling.py
@@ -3,6 +3,8 @@ import pandas as pd
 import os
 # Load the regular expression library
 import re
+import numpy as np
+import PIL
 
 # Import the wordcloud library
 from wordcloud import WordCloud
@@ -13,45 +15,55 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 import gensim.corpora as corpora
+#
+# from pprint import pprint
 
-from pprint import pprint
-
-import pyLDAvis.gensim
-import pickle
 import pyLDAvis
-
-os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Literature\WOS_ModalChoice_Ref\CrossrefResults")
-
-# Read data into papers
-papers = pd.read_csv('./data/NIPS Papers/papers.csv')
-# Print head
-papers.head()
-# Remove the columns
-papers = papers.drop(columns=['id', 'event_type', 'pdf_name'], axis=1).sample(100)
-# Print out the first rows of papers
-papers.head()
+#import pyLDAvis.gensim
+import pickle
 
 
+# os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Literature\WOS_ModalChoice_Ref\CrossrefResults")
+os.chdir(r"C:\Dokumente\PhD EXPANSE\Literature\WOSref\CrossrefResults\txt")
+
+listOfFiles = os.listdir(path='C:/Dokumente/PhD EXPANSE/Literature/WOSref/CrossrefResults/txt')
+print(listOfFiles)
+
+text_df = pd.DataFrame(columns= ["DOI", "text"])
+text_df['DOI'] = listOfFiles
+#text_df['DOI'] = text_df['DOI'].replace(".txt", "")
+print(text_df)
+
+for textdoc in listOfFiles:
+    print(listOfFiles.index(textdoc))
+    text_df.iloc[listOfFiles.index(textdoc), 0] = text_df.iloc[listOfFiles.index(textdoc), 0].replace(".txt", "")
+    text_df.iloc[listOfFiles.index(textdoc), 1] = str(open(textdoc, 'r').read())
+
+print(text_df.head())
+#csv = os.path.join(os.getcwd(),"textcsv.csv")
+csv = r"C:\Dokumente\PhD EXPANSE\Literature\WOSref\CrossrefResults\textcsv.csv"
+pd.DataFrame(text_df).to_csv(csv)
+#
 # Remove punctuation
-papers['paper_text_processed'] = \
-papers['paper_text'].map(lambda x: re.sub('[,\.!?]', '', x))
+text_df['paper_text_processed'] = \
+text_df['text'].map(lambda x: re.sub('[,\.!?]', '', x))
 # Convert the titles to lowercase
-papers['paper_text_processed'] = \
-papers['paper_text_processed'].map(lambda x: x.lower())
+text_df['paper_text_processed'] = \
+text_df['paper_text_processed'].map(lambda x: x.lower())
 # Print out the first rows of papers
-papers['paper_text_processed'].head()
+text_df['paper_text_processed'].head()
 
 
-# Join the different processed titles together.
-long_string = ','.join(list(papers['paper_text_processed'].values))
+ # Join the different processed titles together.
+long_string = ','.join(list(text_df.iloc[0, 2]))
 # Create a WordCloud object
 wordcloud = WordCloud(background_color="white", max_words=5000, contour_width=3, contour_color='steelblue')
 # Generate a word cloud
 wordcloud.generate(long_string)
 # Visualize the word cloud
 wordcloud.to_image()
-
-
+#
+#
 stop_words = stopwords.words('english')
 stop_words.extend(['from', 'subject', 're', 'edu', 'use'])
 def sent_to_words(sentences):
@@ -61,7 +73,7 @@ def sent_to_words(sentences):
 def remove_stopwords(texts):
     return [[word for word in simple_preprocess(str(doc))
              if word not in stop_words] for doc in texts]
-data = papers.paper_text_processed.values.tolist()
+data = text_df.paper_text_processed.values.tolist()
 data_words = list(sent_to_words(data))
 # remove stop words
 data_words = remove_stopwords(data_words)
@@ -77,30 +89,30 @@ corpus = [id2word.doc2bow(text) for text in texts]
 # View
 print(corpus[:1][0][:30])
 
+#
+# # number of topics
+# num_topics = 10
+# # Build LDA model
+# lda_model = gensim.models.LdaMulticore(corpus=corpus,
+#                                        id2word=id2word,
+#                                        num_topics=num_topics)
+# # Print the Keyword in the 10 topics
+# pprint(lda_model.print_topics())
+# doc_lda = lda_model[corpus]
 
-# number of topics
-num_topics = 10
-# Build LDA model
-lda_model = gensim.models.LdaMulticore(corpus=corpus,
-                                       id2word=id2word,
-                                       num_topics=num_topics)
-# Print the Keyword in the 10 topics
-pprint(lda_model.print_topics())
-doc_lda = lda_model[corpus]
-
-
-
-# Visualize the topics
-pyLDAvis.enable_notebook()
-LDAvis_data_filepath = os.path.join('./results/ldavis_prepared_'+str(num_topics))
-# # this is a bit time consuming - make the if statement True
-# # if you want to execute visualization prep yourself
-if 1 == 1:
-    LDAvis_prepared = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
-    with open(LDAvis_data_filepath, 'wb') as f:
-        pickle.dump(LDAvis_prepared, f)
-# load the pre-prepared pyLDAvis data from disk
-with open(LDAvis_data_filepath, 'rb') as f:
-    LDAvis_prepared = pickle.load(f)
-pyLDAvis.save_html(LDAvis_prepared, './results/ldavis_prepared_'+ str(num_topics) +'.html')
-LDAvis_prepared
+#
+#
+# # Visualize the topics
+# pyLDAvis.enable_notebook()
+# LDAvis_data_filepath = os.path.join('./results/ldavis_prepared_'+str(num_topics))
+# # # this is a bit time consuming - make the if statement True
+# # # if you want to execute visualization prep yourself
+# if 1 == 1:
+#     LDAvis_prepared = pyLDAvis.gensim.prepare(lda_model, corpus, id2word)
+#     with open(LDAvis_data_filepath, 'wb') as f:
+#         pickle.dump(LDAvis_prepared, f)
+# # load the pre-prepared pyLDAvis data from disk
+# with open(LDAvis_data_filepath, 'rb') as f:
+#     LDAvis_prepared = pickle.load(f)
+# pyLDAvis.save_html(LDAvis_prepared, './results/ldavis_prepared_'+ str(num_topics) +'.html')
+# LDAvis_prepared
