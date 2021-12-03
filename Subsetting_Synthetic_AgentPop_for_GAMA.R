b0c2531436f517a9da9319f255028d656b5fec85
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2021-12-03 15:25:08 +0100

    Update Subsetting_Synthetic_AgentPop_for_GAMA.R

--- Subsetting_Synthetic_AgentPop_for_GAMA.R
+++ Subsetting_Synthetic_AgentPop_for_GAMA.R
@@ -1,4 +1,4 @@
-Agent_pop = read.csv(paste("C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/Population/", filename, sep = ""), header = T)
+Agent_pop = read.csv(paste("C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/", filename, sep = ""), header = T)
 Agent_pop = Agent_pop[sample(nrow(Agent_pop)),]
 Agent_pop = Agent_pop[0:nb_humans,]
 write.csv2(Agent_pop, "C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/Population/Agent_pop_GAMA.csv", row.names = F ,  quote=FALSE)
