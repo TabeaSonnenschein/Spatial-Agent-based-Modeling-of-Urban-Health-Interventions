f15447eac40c0fe5e8714a1cf8f507c07b818837
Marco Pellegrino <mr.marcopelle@gmail.com>
2022-01-26 15:13:41 +0100

    dataset prepared for regression

--- ODIN regression analysis.R
+++ ODIN regression analysis.R
@@ -63,8 +63,23 @@ ODIN_df <- ODIN_df[ODIN_df$dep_postcode!=1110,] # todo
 ODIN_df <- ODIN_df[ODIN_df$arr_postcode!=1110,] # todo
 
 ODIN_df <- ODIN_df[!is.na(ODIN_df$disp_id),] # delete no trip rows
-ODIN_PC4stats = merge(ODIN_df, postcode_polys@data, by.x = "dep_postcode", by.y = "PC4", all.x = T, all.y = F)
+df <- merge(ODIN_df, postcode_polys@data, by.x = "dep_postcode", by.y = "PC4", all.x = T, all.y = F)
 
+df <- df %>%
+  select(
+    dep_postcode,
+    arr_postcode,
+    sex,
+    age,
+    income_household,
+    has_driving_license,
+    car_in_household,
+    popDns,
+    retaiDns,
+    greenCovr,
+    pubTraDns,
+    RdIntrsDns
+  )
 
 ###
 # In practice: trip distance you need geodesic euclidean distance of the origin postcode centroid to destination postcode centroid
@@ -81,19 +96,19 @@ coord_postcodes <- cbind(PC4 = rownames(coord_postcodes), coord_postcodes)
 rownames(coord_postcodes) <- 1:nrow(coord_postcodes)
 
 ## you need to change the column names of the origin and destination Postcodes of trips
-ODIN_PC4stats$trip_distance <- NA
-ODIN_PC4stats$dep_postcode <- as.character(ODIN_PC4stats$dep_postcode)
-ODIN_PC4stats$arr_postcode <- as.character(ODIN_PC4stats$arr_postcode)
+df$trip_distance <- NA
+df$dep_postcode <- as.character(df$dep_postcode)
+df$arr_postcode <- as.character(df$arr_postcode)
 
 library('geosphere')
-for(trip in 1:nrow(ODIN_PC4stats)){
-  ODIN_PC4stats[trip,"trip_distance"] <- distm(c(coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "dep_postcode"],2], coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "dep_postcode"],3]),
-                                              c(coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "arr_postcode"],2], coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "arr_postcode"],3]),
+for(trip in 1:nrow(df)){
+  df[trip,"trip_distance"] <- distm(c(coord_postcodes[coord_postcodes$PC4==df[trip, "dep_postcode"],2], coord_postcodes[coord_postcodes$PC4==df[trip, "dep_postcode"],3]),
+                                              c(coord_postcodes[coord_postcodes$PC4==df[trip, "arr_postcode"],2], coord_postcodes[coord_postcodes$PC4==df[trip, "arr_postcode"],3]),
                                               fun = distHaversine)
-  ODIN_PC4stats[trip,"dep_x"] <- (coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "dep_postcode"],2])
-  ODIN_PC4stats[trip,"dep_y"] <- (coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "dep_postcode"],3])
-  ODIN_PC4stats[trip,"arr_x"] <- (coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "arr_postcode"],2])
-  ODIN_PC4stats[trip,"arr_y"] <- (coord_postcodes[coord_postcodes$PC4==ODIN_PC4stats[trip, "arr_postcode"],3])
+  df[trip,"dep_x"] <- (coord_postcodes[coord_postcodes$PC4==df[trip, "dep_postcode"],2])
+  df[trip,"dep_y"] <- (coord_postcodes[coord_postcodes$PC4==df[trip, "dep_postcode"],3])
+  df[trip,"arr_x"] <- (coord_postcodes[coord_postcodes$PC4==df[trip, "arr_postcode"],2])
+  df[trip,"arr_y"] <- (coord_postcodes[coord_postcodes$PC4==df[trip, "arr_postcode"],3])
 }
 
 
@@ -101,25 +116,29 @@ for(trip in 1:nrow(ODIN_PC4stats)){
 ###
 #generate affordability based on trip distance and cost as in simulation
 budget = runif(1, 800.0, 5000.0)
-transport_costs = cbind(modal_choice=c('walk', 'bike', 'car'), modal_choice_cost=c(0, 0.01, 0.2))
-transport_costs = as.data.frame(transport_costs)
-transport_costs$modal_choice_cost <- as.numeric(transport_costs$modal_choice_cost)
-ODIN_PC4stats = merge(ODIN_PC4stats, transport_costs, by='modal_choice')
 
-ODIN_PC4stats$affordability = (ODIN_PC4stats$modal_choice_cost*(ODIN_PC4stats$trip_distance/1000))/((budget/31)-20) 
+df$transport_costs_walk <- 0
+df$transport_costs_bike <- 0.01
+df$transport_costs_car <- 0.2
+
+df$affordability_walk = (df$transport_costs_walk*(df$trip_distance/1000))/((budget/31)-20)
+df$affordability_bike = (df$transport_costs_bike*(df$trip_distance/1000))/((budget/31)-20) 
+df$affordability_car = (df$transport_costs_car*(df$trip_distance/1000))/((budget/31)-20) 
+
 
 ###
 #generate trip time based on trip distance and mode speed as in simulation
-transport_speeds = cbind(modal_choice=c('walk', 'bike', 'car'), modal_choice_speed=c(1.4, 3.33, 11.11))
-transport_speeds = as.data.frame(transport_speeds)
-transport_speeds$modal_choice_speed <- as.numeric(transport_speeds$modal_choice_speed)
-ODIN_PC4stats = merge(ODIN_PC4stats, transport_speeds, by='modal_choice')
+df$transport_speed_walk <- 1.4
+df$transport_speed_bike <- 3.33
+df$transport_speed_car <- 11.11
 
-ODIN_PC4stats$trip_time = ODIN_PC4stats$trip_distance/ODIN_PC4stats$modal_choice_speed
+df$travel_time_walk = df$trip_distance/df$transport_speed_walk
+df$travel_time_bike = df$trip_distance/df$transport_speed_bike
+df$travel_time_car = df$trip_distance/df$transport_speed_car
+
 
 ###
 ## trip distance BMI and age interaction just create a interaction within the regression (multiply the variables...)
-
 
 
 ## find the grid cells that intersect the trip and average their environmental attributes
@@ -127,5 +146,136 @@ ODIN_PC4stats$trip_time = ODIN_PC4stats$trip_distance/ODIN_PC4stats$modal_choice
 #pkgs <- c("osrm", "sf")
 #sapply(pkgs, require, character.only = T) #load
 
-#route <- osrmRoute(src = c(ODIN_PC4stats[1,'dep_x'],ODIN_PC4stats[1,'dep_y']), dst = c(ODIN_PC4stats[1,'arr_x'],ODIN_PC4stats[1,'arr_y']), overview = "full", returnclass = "sf", osrm.server = "http://127.0.0.1:5001/", osrm.profile = "bike")
+#route <- osrmRoute(src = c(df[1,'dep_x'],df[1,'dep_y']), dst = c(df[1,'arr_x'],df[1,'arr_y']), overview = "full", returnclass = "sf", osrm.server = "http://127.0.0.1:5001/", osrm.profile = "bike")
 #track = as.data.frame(cbind(unlist(route$geometry)[1:(length(unlist(route$geometry))/2)], unlist(route$geometry)[((length(unlist(route$geometry))/2)+1):length(unlist(route$geometry))]))
+
+
+### calculate utilities
+# fix NaN environmental variables
+df[is.nan(df$popDns),]$popDns <- 0
+df[is.nan(df$retaiDns),]$retaiDns <- 0
+df[is.nan(df$greenCovr),]$greenCovr <- 0
+df[is.nan(df$pubTraDns),]$pubTraDns <- 0
+df[is.nan(df$RdIntrsDns),]$RdIntrsDns <- 0
+
+# normalise factors
+normalise <- function(a){
+  return((a-min(a))/(max(a)-min(a)))
+}
+
+df$affordability_walk <- 1-tanh(df$affordability_walk)
+df$affordability_bike <- 1-tanh(df$affordability_bike)
+df$affordability_car <- 1-tanh(df$affordability_car)
+df$travel_time_walk <- 1-tanh(df$travel_time_walk/1000)
+df$travel_time_bike <- 1-tanh(df$travel_time_bike/1000)
+df$travel_time_car <- 1-tanh(df$travel_time_car/1000)
+df$trip_distance <- 1-tanh(df$trip_distance/1000)
+df$popDns <- normalise(df$popDns)
+df$retaiDns <- normalise(df$retaiDns)
+df$greenCovr <- normalise(df$greenCovr)
+df$pubTraDns <- normalise(df$pubTraDns)
+df$RdIntrsDns <- normalise(df$RdIntrsDns)
+
+df$utility_walk <-
+  df$affordability_walk + # always zero anyway
+  df$popDns +
+  df$retaiDns +
+  df$greenCovr +
+  df$pubTraDns +
+  df$RdIntrsDns +
+  df$travel_time_walk +
+  df$trip_distance
+df$utility_walk <- df$utility_walk/8
+
+df$utility_bike <-
+  df$affordability_bike +
+  df$popDns +
+  df$retaiDns +
+  df$greenCovr +
+  df$pubTraDns +
+  df$RdIntrsDns +
+  df$travel_time_bike +
+  df$trip_distance
+df$utility_bike <- df$utility_bike/8
+
+df$utility_car <-
+  df$affordability_car +
+  df$travel_time_car +
+  df$trip_distance
+df$utility_car <- df$utility_car/3
+
+# select modal choice also based on age limitations
+df$predicted_modal_choice <- NA
+
+for(i in 1:nrow(df)){
+  if (df[i,"age"]<=8){
+    df[i, 'predicted_modal_choice'] <- 'walk'
+  } else if (df[i,"age"]>=8 & df[i,"age"]<=17) {
+    df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_walk'] > df[i, 'utility_bike'],
+                                              "walk", df[i, 'predicted_modal_choice'])
+    df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_bike'] > df[i, 'utility_walk'],
+                                              "bike", df[i, 'predicted_modal_choice'])
+  } else { # adult
+    if (df[i,"has_car_license"]==1 && df[i,"car_in_household"]==1) {
+      df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_walk'] > df[i, 'utility_bike'] &
+                                                  df[i, 'utility_walk'] > df[i, 'utility_car'],
+                                                "walk", df[i, 'predicted_modal_choice'])
+      df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_bike'] > df[i, 'utility_walk'] &
+                                                  df[i, 'utility_bike'] > df[i, 'utility_car'],
+                                                "bike", df[i, 'predicted_modal_choice'])
+      df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_car'] > df[i, 'utility_bike'] &
+                                                  df[i, 'utility_car'] > df[i, 'utility_bike'],
+                                                "car", df[i, 'predicted_modal_choice']) 
+    } else {
+      df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_walk'] > df[i, 'utility_bike'],
+                                                "walk", df[i, 'predicted_modal_choice'])
+      df[i, 'predicted_modal_choice'] <- ifelse(df[i, 'utility_bike'] > df[i, 'utility_walk'],
+                                                "bike", df[i, 'predicted_modal_choice'])
+    }
+  }
+}
+
+df <- df %>%
+  select(
+    predicted_modal_choice,
+    affordability_walk,
+    affordability_bike,
+    affordability_car,
+    travel_time_walk,
+    travel_time_bike,
+    travel_time_car,
+    trip_distance,
+    popDns,
+    retaiDns,
+    greenCovr,
+    pubTraDns,
+    RdIntrsDns
+  )
+
+# subselect factors for regression
+df$affordability = ifelse(df$predicted_modal_choice=='walk',
+                          df$affordability_walk,
+                          ifelse(df$predicted_modal_choice=='bike',
+                                 df$affordability_bike,
+                                 df$affordability_car))
+
+df$travel_time = ifelse(df$predicted_modal_choice=='walk',
+                          df$travel_time_walk,
+                          ifelse(df$predicted_modal_choice=='bike',
+                                 df$travel_time_bike,
+                                 df$travel_time_car))
+
+df <- df %>%
+  select(
+    predicted_modal_choice,
+    affordability,
+    travel_time,
+    trip_distance,
+    popDns,
+    retaiDns,
+    greenCovr,
+    pubTraDns,
+    RdIntrsDns
+  )
+
+
