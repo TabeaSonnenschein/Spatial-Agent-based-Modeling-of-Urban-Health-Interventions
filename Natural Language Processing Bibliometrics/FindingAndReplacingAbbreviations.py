80753ba3f87d9e6e9ee291a02e6f6d75daba7431
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2022-02-24 15:30:43 +0100

    x

--- Natural Language Processing Bibliometrics/FindingAndReplacingAbbreviations.py
+++ Natural Language Processing Bibliometrics/FindingAndReplacingAbbreviations.py
@@ -9,34 +9,35 @@ def FindAbbrev(sentence):
         if ("(" and ")" in sentence):
             open_brackets = sentence.find("(")
             close_brackets = sentence.find(")")
-            if sentence[open_brackets + 1:close_brackets].isupper() and sentence[
-                                                                      open_brackets + 1:close_brackets].isalpha() and (
-                    len(sentence[open_brackets + 1:close_brackets]) > 1):
-                abbr = sentence[open_brackets + 1:close_brackets]
-                abbreviation_len = len(abbr)
-                words_before = re.split(' |-|\n|\+', sentence[:open_brackets].strip())
-                words_before = list(filter(None, words_before))
-                print(words_before)
-                words_before = words_before[len(words_before) - abbreviation_len - 3:]
-                possible_fullnames = []
-                if len(abbr) < 2:
-                    for count, value in enumerate(words_before[:len(words_before) - 2]):
-                        if value[0].upper() == abbr[0] and (
-                                words_before[count + 1][0].upper() == abbr[1] or words_before[count + 2][0].upper() ==
-                                abbr[1]):
-                            fullname = " ".join(words_before[count:])
-                            possible_fullnames.append(fullname)
-                else:
-                    for count, value in enumerate(words_before[:len(words_before) - 1]):
-                        if value[0].upper() == abbr[0] and words_before[count + 1][0].upper() == abbr[1]:
-                            fullname = " ".join(words_before[count:])
-                            possible_fullnames.append(fullname)
-                if bool(possible_fullnames):
-                    final_fullname = min(possible_fullnames, key=len)
-                    new_sentence = "".join([sentence[:open_brackets], sentence[close_brackets + 1:]])
-                    return new_sentence, abbr, final_fullname
-                else:
-                    return sentence, "", ""
+            open_brackets = [m.start() for m in re.finditer("[(]", sentence)]
+            close_brackets = [m.start() for m in re.finditer("[)]", sentence)]
+            if sentence[open_brackets + 1:close_brackets].isupper():
+                if sentence[open_brackets + 1:close_brackets].isalpha() and (len(sentence[open_brackets + 1:close_brackets]) > 1):
+                    abbr = sentence[open_brackets + 1:close_brackets]
+                    abbreviation_len = len(abbr)
+                    words_before = re.split(' |-|\n|\+', sentence[:open_brackets].strip())
+                    words_before = list(filter(None, words_before))
+                    print(words_before)
+                    words_before = words_before[len(words_before) - abbreviation_len - 3:]
+                    possible_fullnames = []
+                    if len(abbr) < 2:
+                        for count, value in enumerate(words_before[:len(words_before) - 2]):
+                            if value[0].upper() == abbr[0] and (
+                                    words_before[count + 1][0].upper() == abbr[1] or words_before[count + 2][0].upper() ==
+                                    abbr[1]):
+                                fullname = " ".join(words_before[count:])
+                                possible_fullnames.append(fullname)
+                    else:
+                        for count, value in enumerate(words_before[:len(words_before) - 1]):
+                            if value[0].upper() == abbr[0] and words_before[count + 1][0].upper() == abbr[1]:
+                                fullname = " ".join(words_before[count:])
+                                possible_fullnames.append(fullname)
+                    if bool(possible_fullnames):
+                        final_fullname = min(possible_fullnames, key=len)
+                        new_sentence = "".join([sentence[:open_brackets], sentence[close_brackets + 1:]])
+                        return new_sentence, abbr, final_fullname
+                    else:
+                        return sentence, "", ""
             else:
                 return sentence, "", ""
         else:
--- Natural Language Processing Bibliometrics/XMLToCleanText.py
+++ Natural Language Processing Bibliometrics/XMLToCleanText.py
@@ -34,8 +34,8 @@ for file in listOfFiles:
     for count, value in enumerate(label_ends):
         fulltext += xml_file[(value+1):label_starts[count+1]].replace("\n", "").strip() + " "
     cleantext, abbreviations, fullnames = FindNReplaceAbbr(fulltext)
-    cleantext = cleantext.replace(" %", "%").replace("i. e.", "id est").replace("e. g.", "for example").replace("al. ", "al").replace("AL. ", "al")
-    cleantext = cleantext.replace("Fig. ", "Figure ").replace("Tab. ", "Table ").replace("p. ", "p").replace(" vs. ", " versus ")
+    cleantext = cleantext.replace(" %", "%").replace("i. e.", "id est").replace("e. g.", "for example").replace("al. ", "al").replace("AL. ", "al").replace("i.e.", "id est").replace("e.g.", "for example")
+    cleantext = cleantext.replace("Fig. ", "Figure ").replace("Tab. ", "Table ").replace("p. ", "p").replace(" vs. ", " versus ").replace("pp.", "pp").replace("Vol.", "Vol").replace("No.", "No")
     file1.writelines(cleantext.strip())
     full_abbr.extend(abbreviations)
     full_fullnames.extend(fullnames)
