6e703abbfe0a330433d1e2d6a787a704ede6f1eb
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2022-05-17 23:34:08 +0200

    Update syntactical_relation_learning_model.py

--- Natural Language Processing Bibliometrics/syntactical_relation_learning_model.py
+++ Natural Language Processing Bibliometrics/syntactical_relation_learning_model.py
@@ -18,36 +18,56 @@ from sklearn.metrics import f1_score
 from matplotlib import pyplot
 
 os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
-csv = os.path.join(os.getcwd(), ("possible_evidence_instances_training_data.csv"))
-data = pd.read_csv(csv)
+csv = os.path.join(os.getcwd(), ("possible_evidence_instances_training_data600.csv"))
+traindata = pd.read_csv(csv)
 # replace Nan by -100
-data.replace({'NaN': -100}, regex=True, inplace=True)
+traindata.replace({'NaN': -100}, regex=True, inplace=True)
 
-print(data.iloc[:500,9:].head())
+print(traindata.iloc[:500,9:].head())
 
 # define dataset
 # X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_redundant=5, random_state=1)
-y = data['Evidence_Truth'].iloc[:500]
-X = data.iloc[:500,9:]
+y = traindata['Evidence_Truth'].iloc[:500]
+X = traindata.iloc[:500,9:]
 
 # evaluate the model
 model = GradientBoostingClassifier()
 cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
 n_scores = cross_val_score(model, X, y, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
 print('Accuracy: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
-
+n_scores = cross_val_score(model, X, y, scoring='f1_weighted', cv=cv, n_jobs=-1, error_score='raise')
+print('F1 Score: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
 
 # fit the model on the whole dataset
 model = GradientBoostingClassifier()
 model.fit(X, y)
 
-# f1_scores_perclass = f1_score(y_true=valid_tags, y_pred=pred_tags, average=None, labels=unique_labels)
-# f1_weighted_mean = f1_score(y_true=valid_tags, y_pred=pred_tags, average='weighted', labels=unique_labels)
 
-# # make a single prediction
-# row = [[2.56999479, -0.13019997, 3.16075093, -4.35936352, -1.61271951, -1.39352057, -2.48924933, -1.93094078, 3.26130366, 2.05692145]]
-# yhat = model.predict(row)
-# print('Prediction: %d' % yhat[0])
+
+# # make predictions on rest of evidence instances
+csv = os.path.join(os.getcwd(), ("possible_evidence_instances.csv"))
+preddata = pd.read_csv(csv)
+preddata.replace({'NaN': -100}, regex=True, inplace=True)
+print(preddata.iloc[:500,9:].head())
+
+predictions =[]
+for i in range(0, len(preddata)):
+    predictions.extend(model.predict([preddata.iloc[i,9:]]))
+preddata["Evidence_Truth"] = predictions
+print(predictions[0:1000])
+
+preddata.to_csv("predicted_evidence_instances_all.csv", index=False)
+
+trueevidenceintances = preddata.iloc[list(np.where(preddata["Evidence_Truth"] == 1)[0])]
+trueevidenceintances.to_csv("predicted_evidence_instances_true.csv", index=False)
+
+
+
+
+
+
+
+
 
 # random forest, support vector model, neural network
 # Supervised classification model
