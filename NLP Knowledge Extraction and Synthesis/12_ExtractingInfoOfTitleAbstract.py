a459a259e205bae0adc928bea2f4572fe25be57b
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2022-06-20 15:52:44 +0200

    Create 12_ExtractingInfoOfTitleAbstract.py

---
+++ NLP Knowledge Extraction and Synthesis/12_ExtractingInfoOfTitleAbstract.py
@@ -0,0 +1,105 @@
+import os
+import pandas as pd
+import numpy as np
+from itertools import chain
+
+pd.options.display.max_colwidth = 100000
+
+os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper\modalchoice literature search")
+article_data = pd.read_csv("metareview_details.csv")
+article_data['filename'] = [( doi.replace("/", "_")) for doi in article_data['doi']]
+
+os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
+labeled_words = pd.read_csv("Evidence_instances_df.csv")
+labeled_words['filename'] = [doi.replace(".csv", "").replace("doi_", "") for doi in labeled_words['DOI']]
+evidence_instances = pd.read_csv("unique_evidence_instances.csv")
+evidence_instances['DOI'] = [doi.replace(".csv", "").replace("doi_", "") for doi in evidence_instances['DOI']]
+
+def extendInfofromTitleAbstract(articlesdata, fulllabeledtext, evidenceinstance_df):
+    labeled_papers = list(dict.fromkeys(evidenceinstance_df['DOI']))
+    evidenceinstance_df[['Title', 'Abstract', 'NrBO_inArticle', 'NrTitleBOappears', 'NrSG_inArticle', 'BehavOptionsInTitle', 'BehavOptionsInAbstract', 'StudyGroupInTitle', 'StudyGroupInAbstract']] = ""
+    for file in labeled_papers:
+        article_data_idx = list(np.where(articlesdata['filename'] == file)[0])
+        evidence_data_idx = list(np.where(evidenceinstance_df['DOI'] == file)[0])
+        labeled_words_idx = list(np.where(fulllabeledtext['filename'] == file)[0])
+        title = articlesdata['title'].iloc[article_data_idx].to_string()
+        evidenceinstance_df['Title'].iloc[evidence_data_idx] = title
+        title = title.lower()
+        BehavOptions = fulllabeledtext['BehaviorOption'].iloc[labeled_words_idx]
+        BehavOptions = [elm.lower().split(" ; ") for elm in BehavOptions if isinstance(elm, str)]
+        BehavOptions = list(dict.fromkeys(list(chain.from_iterable(BehavOptions))))
+        evidenceinstance_df['NrBO_inArticle'].iloc[evidence_data_idx] = len(BehavOptions)
+        BO_inTitle = [elm for elm in BehavOptions if title.find(elm) != -1]
+        if len(BO_inTitle) > 1:
+            longest_BO = max(BO_inTitle, key=len)
+            if all(True for elm in BO_inTitle if elm in longest_BO):
+                BO_inTitle = [longest_BO]
+        evidenceinstance_df['NrTitleBOappears'].iloc[evidence_data_idx] = len([i for i in fulllabeledtext['BehaviorOption'].iloc[labeled_words_idx] if [i] == BO_inTitle])
+        evidenceinstance_df['BehavOptionsInTitle'].iloc[evidence_data_idx] = " ; ".join(BO_inTitle)
+        abstract = article_data['abstract'].iloc[article_data_idx].to_string().lower()
+        evidenceinstance_df['Abstract'].iloc[evidence_data_idx] = abstract
+        print(title)
+        print(abstract)
+        print(BO_inTitle)
+        if BO_inTitle == []:
+            BO_inAbstract = [elm for elm in BehavOptions if abstract.find(elm) != -1]
+            if len(BO_inAbstract) > 1:
+                longest_BO = max(BO_inAbstract, key=len)
+                if all(True for elm in BO_inAbstract if elm in longest_BO):
+                    BO_inAbstract = [longest_BO]
+            evidenceinstance_df['BehavOptionsInAbstract'].iloc[evidence_data_idx] = " ; ".join(BO_inAbstract)
+            print(BO_inAbstract)
+        StudyGroup = fulllabeledtext['Studygroup'].iloc[labeled_words_idx]
+        StudyGroup = [elm.lower().split(" ; ") for elm in StudyGroup if isinstance(elm, str)]
+        StudyGroup = list(dict.fromkeys(list(chain.from_iterable(StudyGroup))))
+        evidenceinstance_df['NrSG_inArticle'].iloc[evidence_data_idx] = len(StudyGroup)
+        SG_inTitle = [elm for elm in StudyGroup if title.find(elm) != -1]
+        if len(SG_inTitle) > 1:
+            longest_SG = max(SG_inTitle, key=len)
+            if all(True for elm in SG_inTitle if elm in longest_SG):
+                SG_inTitle = [longest_SG]
+        evidenceinstance_df['StudyGroupInTitle'].iloc[evidence_data_idx] = " ; ".join(SG_inTitle)
+        print(SG_inTitle)
+        if SG_inTitle == []:
+            SG_inAbstract = [elm for elm in StudyGroup if abstract.find(elm) != -1]
+            if len(SG_inAbstract) > 1:
+                longest_SG = max(SG_inAbstract, key=len)
+                if all(True for elm in SG_inAbstract if elm in longest_SG):
+                    SG_inAbstract = [longest_SG]
+            evidenceinstance_df['StudyGroupInAbstract'].iloc[evidence_data_idx] = " ; ".join(SG_inAbstract)
+            print(SG_inAbstract)
+
+    evidenceinstance_df['Title'] = [title.replace("Series([], )", "") for title in evidence_instances['Title']]
+    return evidenceinstance_df
+
+
+def find_lastBO_for_missing_BO_info(fulllabeledtext, evidenceinstance_df):
+    evidenceinstance_df[['last_BO', 'sent_last_BO', 'sent_distance_lastBO']] = " "
+    for count, value in enumerate(evidenceinstance_df['Sentence']):
+        if str(evidenceinstance_df['BehaviorOption'].iloc[count]) == "-100":
+            labeled_words_idx = list(np.where(fulllabeledtext['filename'] == evidenceinstance_df['DOI'].iloc[count])[0])
+            current_idx = list(np.where((fulllabeledtext['filename'] == evidenceinstance_df['DOI'].iloc[count]) & (fulllabeledtext['Sentence'] == evidenceinstance_df['Sentence'].iloc[count]))[0])
+            potentialBO = [i for i in fulllabeledtext['BehaviorOption'].iloc[min(labeled_words_idx): min(current_idx)] if str(i) != "nan"]
+            sentenceID_BO = [val for cnt, val in enumerate(fulllabeledtext['Sentence'].iloc[min(labeled_words_idx):min(current_idx)]) if str(fulllabeledtext['BehaviorOption'].iloc[labeled_words_idx[cnt-1]]) != "nan"]
+            if len(potentialBO) > 0:
+                evidenceinstance_df['last_BO'].iloc[count] = potentialBO[-1]
+                evidenceinstance_df['sent_last_BO'].iloc[count] = sentenceID_BO[-1]
+                evidenceinstance_df['sent_distance_lastBO'].iloc[count] = int(evidenceinstance_df['Sentence'].iloc[count].replace("Sentence: ", "")) - int(evidenceinstance_df['sent_last_BO'].iloc[count].replace("Sentence: ", ""))
+                print(potentialBO[-1])
+    return evidenceinstance_df
+
+def select_correct_imputation(evidenceinstance_df):
+    for count, value in enumerate(evidenceinstance_df['BehaviorOption']):
+        if value == "-100":
+            if evidenceinstance_df['BehavOptionsInTitle'].iloc[count] == evidenceinstance_df['last_BO'].iloc[count]:
+                evidenceinstance_df['BehaviorOption'].iloc[count] = evidenceinstance_df['BehavOptionsInTitle'].iloc[count]
+            # elif
+    return evidenceinstance_df
+
+evidence_instances = extendInfofromTitleAbstract(articlesdata = article_data, fulllabeledtext = labeled_words, evidenceinstance_df= evidence_instances)
+evidence_instances = find_lastBO_for_missing_BO_info(fulllabeledtext = labeled_words, evidenceinstance_df= evidence_instances)
+evidence_instances = select_correct_imputation(evidenceinstance_df = evidence_instances)
+
+csv = os.path.join(os.getcwd(), ("unique_evidence_instances_interpolatingMissinginfo.csv"))
+evidence_instances.to_csv(csv, index=False)
+
