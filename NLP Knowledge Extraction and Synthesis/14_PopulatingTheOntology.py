c05c39eb3ccd8f94e4cb3a788dff054f7d2085b2
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2022-12-13 01:14:20 +0100

    x

--- NLP Knowledge Extraction and Synthesis/14_PopulatingTheOntology.py
+++ NLP Knowledge Extraction and Synthesis/14_PopulatingTheOntology.py
@@ -1,7 +1,8 @@
 from sys import platform
 from owlready2 import *
-from numpy import *
+import numpy as np
 import pandas as pd
+import types
 import rdflib
 from rdflib.namespace import NamespaceManager
 from rdflib import BNode, Namespace, Graph
@@ -17,38 +18,12 @@ if platform == 'win32':
 os.chdir(r"D:\PhD EXPANSE\Written Paper\02- Behavioural Model paper\modalchoice literature search")
 meta_review_details = pd.read_csv("metareview_details_short.csv")
 
-
 # # Synchronise the reasoner in order to get inferences
 # sync_reasoner_pellet(onto, True, True, 0)
 
-## adding study details
-print(onto.Studies.instances())
-
-for count, value in enumerate(meta_review_details['doi']):
-    print(count, value)
-    paper_doi = onto.search(iri="*" + value)
-    print("ontology_paper_doi" + str(paper_doi))
-    if paper_doi:
-        paper_doi = paper_doi[0]
-    else:
-        paper_doi = onto.ReviewStudies(value)
-        if "meta-analy" in str(meta_review_details['Article.Title'].iloc[count]).lower():
-            paper_doi.is_a.append(onto.MetaAnalysis)
-        if "systematic review" in str(meta_review_details['Article.Title'].iloc[count]).lower() or "systematic literature review" in str(meta_review_details['Article.Title'].iloc[count]).lower() :
-            paper_doi.is_a.append(onto.SystematicReviewStudies)
-    paper_doi.HasCitation = [str(meta_review_details['citation'].iloc[count])]
-    paper_doi.HasStudyYear = [int(meta_review_details['Publication.Year'].iloc[count])]
-    paper_doi.HasTitle = [str(meta_review_details['Article.Title'].iloc[count])]
-    paper_doi.HasJournal = [str(meta_review_details['Source.Title'].iloc[count])]
-    paper_doi.comment = [str(meta_review_details['citation'].iloc[count])]
-    for prop in paper_doi.get_properties():
-        for DataPropertyValue in prop[paper_doi]:
-            print(".%s == %s" % (prop.python_name, DataPropertyValue))
-
-
 ## adding evidence instances
 os.chdir(r"D:\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
-evidence_instances_full = pd.read_csv("unique_evidence_instances_clean2.csv")
+evidence_instances_full = pd.read_csv("unique_evidence_instances_clean2_harmonised_BO_manualclean.csv")
 evidence_instances = evidence_instances_full[['DOI', 'Sentence', 'Fullsentence', 'BehaviorOption', 'BehaviorDeterminant', 'Studygroup',
                                               'Moderator', 'stat_significance', 'stat_consistency', 'stat_direction', 'stat_correl']]
 
@@ -59,13 +34,12 @@ uniqueMO = list(dict.fromkeys([x.lower() for x in evidence_instances_full['Moder
 # uniqueBO.remove("-100")
 uniqueSG.remove("-100")
 uniqueMO.remove("-100")
-uniqueSG = list(dict.fromkeys(list(chain.from_iterable([i.replace("[", "").replace("]","").replace("'","").strip().split(", ") for i in uniqueSG]))))
+uniqueSG = list(dict.fromkeys(list(chain.from_iterable([i.replace("[", "").replace("]","").replace("'","").replace("<", "less than").strip().split(", ") for i in uniqueSG]))))
 print(uniqueSG)
 uniqueBO = [el.replace(" ", "_") for el in uniqueBO]
 uniqueSG = [el.replace(" ", "_") for el in uniqueSG]
 uniqueMO = [el.replace(" ", "_") for el in uniqueMO]
 uniqueBD = [el.replace(" ", "_") for el in uniqueBD]
-
 
 for i in uniqueBO:
     x = onto.BehaviorChoiceOptions(i)
@@ -76,25 +50,59 @@ for i in uniqueSG:
 for i in uniqueMO:
     x = onto.BehaviorModerators(i)
 
+# uniqueBO_classes = list(dict.fromkeys([x.lower() for x in evidence_instances_full['BehaviorOptionHarmon']]))
+# uniqueBO_classes = [el.replace(" ", "_") for el in uniqueBO_classes]
+# with onto:
+#     for i in uniqueBO_classes:
+#         print(i)
+#         my_new_class = types.new_class(str(i), (onto["BehaviorChoiceOptions"],))  # make a class
+#     pass
+
 
 # graph = default_world.as_rdflib_graph()
 
 for count, value in enumerate(evidence_instances['DOI']):
+
+    ## Create an evidence instance individual
     # Evid_inst = graph.BNode()  # blanknode
     #     with onto:
     #         graph.add((Evid_inst, rdflib.URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
     #                rdflib.URIRef("http://www.w3.org/2002/07/owl#Class")))
     Evid_inst = onto.EvidenceInstances("ev_inst_" + str(count))
+
+
+    ## identify the article in ontology or add details if not yet exists
     doi = value.replace(".csv", "").replace("_", "/")
     paper_doi = onto.search(iri="*" + doi)
     if paper_doi:
         paper_doi = paper_doi[0]
     else:
+        doi_idx = np.where(meta_review_details['doi'] == doi)[0]
+        print(doi_idx)
         paper_doi = onto.ReviewStudies(doi)
+        if len(doi_idx) > 0:
+            if "meta-analy" in str(meta_review_details['Article.Title'].iloc[doi_idx[0]]).lower():
+                paper_doi.is_a.append(onto.MetaAnalysis)
+            if "systematic review" in str(meta_review_details['Article.Title'].iloc[doi_idx[0]]).lower() \
+                    or "systematic literature review" in str(meta_review_details['Article.Title'].iloc[doi_idx[0]]).lower():
+                paper_doi.is_a.append(onto.SystematicReviewStudies)
+            paper_doi.HasCitation = [str(meta_review_details['citation'].iloc[doi_idx[0]])]
+            paper_doi.HasStudyYear = [int(meta_review_details['Publication.Year'].iloc[doi_idx[0]])]
+            paper_doi.HasTitle = [str(meta_review_details['Article.Title'].iloc[doi_idx[0]])]
+            paper_doi.HasJournal = [str(meta_review_details['Source.Title'].iloc[doi_idx[0]])]
+            paper_doi.comment = [str(meta_review_details['citation'].iloc[doi_idx[0]])]
+        else:
+            paper_doi.comment = [str("https://doi.org/" + doi)]
+        for prop in paper_doi.get_properties():
+            for DataPropertyValue in prop[paper_doi]:
+                print(".%s == %s" % (prop.python_name, DataPropertyValue))
+
+    ## identify the evidence info individuals in the ontology
+    ## and add the object property relations
     BO = onto.search(iri="*" + evidence_instances_full['BehaviorOption'].iloc[count].replace(" ", "_").lower())
     BD = onto.search(iri="*" + evidence_instances_full['BehaviorDeterminant'].iloc[count].replace(" ", "_").lower())
     MO = onto.search(iri="*" + evidence_instances_full['Moderator'].iloc[count].replace(" ", "_").lower())
-    localSGs = evidence_instances_full['Studygroup'].iloc[count].replace("[", "").replace("]","").replace("'","").strip().split(", ")
+    localSGs = evidence_instances_full['Studygroup'].iloc[count].replace("[", "").replace("]","").replace("'","").replace("<", "less than").strip().split(", ")
     print(localSGs)
     if localSGs != ['-100']:
         for localSG in localSGs:
@@ -123,6 +131,8 @@ for count, value in enumerate(evidence_instances['DOI']):
         BO[0].StudiedChoiceOptions.append(Evid_inst)
     if MO:
         MO[0].ModeratorIn.append(Evid_inst)
+
+    ## print the evidence instance properties
     for prop in Evid_inst.get_properties():
         for DataPropertyValue in prop[Evid_inst]:
             print(".%s == %s" % (prop.python_name, DataPropertyValue))
@@ -132,8 +142,8 @@ for count, value in enumerate(evidence_instances['DOI']):
 #### despite following the instructions of the documentations, it does not work
 # sync_reasoner_pellet(onto)
 # sync_reasoner(onto,True, True, 0)
-# with onto:
-#     sync_reasoner_pellet(infer_property_values = True)
+with onto:
+    sync_reasoner_pellet(infer_property_values = True, debug=0)
 
 onto.save(file = os.path.join(os.getcwd(),"extendedBehaviouralModelOntology3.owl"), format = "rdfxml")
 
--- NLP Knowledge Extraction and Synthesis/15_Visualization.py
+++ NLP Knowledge Extraction and Synthesis/15_Visualization.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from networkx.algorithms import bipartite
 #
-os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
+os.chdir(r"D:\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
 evidence_instances_full = pd.read_csv("unique_evidence_instances_clean2.csv")
 evidence_instances_full.replace({"-100": 'NaN'}, regex=False, inplace=True)
 evidence_instances_full.fillna("NaN")
@@ -53,59 +53,63 @@ uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "BehaviorDeterminant", "B
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "Studygroup", "SG_")
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "Moderator", "MO_")
 
-uniq_edgelist.to_csv("full_edgelist.csv", index=False)
+# uniq_edgelist.to_csv("full_edgelist.csv", index=False)
+
+# Output dataframes can be read by gephi and when installing EventGraphLayout plugin transformed to bipartite graph visualization
+def prepare_output_tables_Gephi_subgraphBOBD(df, subset_keys, graph_name, Source_col, Target_col, Source_label, Target_label):
+    sub_graph_edgelist = df.iloc[[count for count, value in enumerate(df["BehaviorOption"]) if any(True for x in subset_keys if x in value)]]
+    sub_graph_edgelist.rename(columns={Source_col: 'Source', Target_col: 'Target'}, inplace=True)
+    nodes = pd.concat([sub_graph_edgelist[['Source', Source_label]].rename(columns={'Source':'Id', Source_label: 'Label' }),
+                       sub_graph_edgelist[['Target', Target_label]].rename(columns={'Target':'Id', Target_label: 'Label' })],
+                      ignore_index=True, axis=0)
+    nodes["Mode"] = 3
+    nodes["Mode"].iloc[:len(sub_graph_edgelist['Source'])] = 2
+    nodes.loc[len(nodes.index)] = ['BO_class', graph_name , 1]
+    nodes = nodes.drop_duplicates()
+    superclass_df = pd.DataFrame({"Source": ['BO_class'] * len(set(sub_graph_edgelist['Source'])),
+                                  "Target": list(dict.fromkeys(sub_graph_edgelist['Source'])),
+                                  "sign_consist": [3]* len(set(sub_graph_edgelist['Source'])),
+                                  "counts_mention": [100]* len(set(sub_graph_edgelist['Source'])),
+                                  "counts_articles": [100]* len(set(sub_graph_edgelist['Source']))})
+    sub_graph_edgelist = pd.concat([sub_graph_edgelist[['Source', 'Target', 'sign_consist', 'counts_mention', 'counts_articles']],
+                                    superclass_df], ignore_index=True, axis=0)
+    sub_graph_edgelist.to_csv(
+        (graph_name + "_edgelist.csv"), index=False)
+    nodes.to_csv((graph_name+"_nodeslist.csv"), index=False)
 #
-# # Output dataframes can be read by gephi and when installing EventGraphLayout plugin transformed to bipartite graph visualization
-# def prepare_output_tables_Gephi_subgraphBOBD(df, subset_keys, graph_name, Source_col, Target_col, Source_label, Target_label):
-#     sub_graph_edgelist = df.iloc[[count for count, value in enumerate(df["BehaviorOption"]) if any(True for x in subset_keys if x in value)]]
-#     sub_graph_edgelist.rename(columns={Source_col: 'Source', Target_col: 'Target'}, inplace=True)
-#     nodes = pd.concat([sub_graph_edgelist[['Source', Source_label]].rename(columns={'Source':'Id', Source_label: 'Label' }),
-#                        sub_graph_edgelist[['Target', Target_label]].rename(columns={'Target':'Id', Target_label: 'Label' })],
-#                       ignore_index=True, axis=0)
-#     nodes["Mode"] = 3
-#     nodes["Mode"].iloc[:len(sub_graph_edgelist['Source'])] = 2
-#     nodes.loc[len(nodes.index)] = ['BO_class', graph_name , 1]
-#     nodes = nodes.drop_duplicates()
-#     superclass_df = pd.DataFrame({"Source": ['BO_class'] * len(set(sub_graph_edgelist['Source'])),
-#                                   "Target": list(dict.fromkeys(sub_graph_edgelist['Source'])),
-#                                   "sign_consist": [3]* len(set(sub_graph_edgelist['Source'])),
-#                                   "counts_mention": [100]* len(set(sub_graph_edgelist['Source'])),
-#                                   "counts_articles": [100]* len(set(sub_graph_edgelist['Source']))})
-#     sub_graph_edgelist = pd.concat([sub_graph_edgelist[['Source', 'Target', 'sign_consist', 'counts_mention', 'counts_articles']],
-#                                     superclass_df], ignore_index=True, axis=0)
-#     sub_graph_edgelist.to_csv(
-#         (graph_name + "_edgelist.csv"), index=False)
-#     nodes.to_csv((graph_name+"_nodeslist.csv"), index=False)
-#
-# def prepare_output_tables_Gephi_subgraph_all(df, subset_keys, graph_name):
-#     sub_graph_edgelist = df.iloc[[count for count, value in enumerate(df["BehaviorOption"]) if any(True for x in subset_keys if x in value)]]
-#     sub_graph_edgelist1 = sub_graph_edgelist[['BO_keys', 'BD_keys', 'sign_consist', 'counts_mention', 'counts_articles']].rename(columns={'BO_keys': 'Source', 'BD_keys': 'Target'})
-#     sub_graph_edgelist2 = sub_graph_edgelist[['BD_keys', 'SG_keys', 'sign_consist', 'counts_mention', 'counts_articles']].rename(columns={'BD_keys': 'Source', 'SG_keys': 'Target'})
-#     nodes = pd.concat([sub_graph_edgelist[['BO_keys', 'BehaviorOption']].rename(columns={'BO_keys':'Id', 'BehaviorOption': 'Label' }),
-#                        sub_graph_edgelist[['BD_keys', 'BehaviorDeterminant']].rename(columns={'BD_keys':'Id', 'BehaviorDeterminant': 'Label' }),
-#                        sub_graph_edgelist[['SG_keys', 'Studygroup']].rename(columns={'SG_keys':'Id', 'Studygroup': 'Label'})],
-#                       ignore_index=True, axis=0)
-#     l = len(sub_graph_edgelist.index)
-#     nodes["Mode"] = 4
-#     nodes["Mode"].iloc[:l] = 2
-#     nodes["Mode"].iloc[l:(l*2)] = 3
-#     nodes.loc[len(nodes.index)] = ['BO_class', graph_name , 1]
-#     nodes = nodes.drop_duplicates()
-#     superclass_df = pd.DataFrame({"Source": ['BO_class'] * len(set(sub_graph_edgelist['BO_keys'])),
-#                                   "Target": list(dict.fromkeys(sub_graph_edgelist['BO_keys'])),
-#                                   "sign_consist": [3]* len(set(sub_graph_edgelist['BO_keys'])),
-#                                   "counts_mention": [100]* len(set(sub_graph_edgelist['BO_keys'])),
-#                                   "counts_articles": [100]* len(set(sub_graph_edgelist['BO_keys']))})
-#     sub_graph_edgelist = pd.concat([sub_graph_edgelist1, sub_graph_edgelist2, superclass_df], ignore_index=True, axis=0)
-#     sub_graph_edgelist.to_csv(
-#         (graph_name + "_edgelist_BOBDSG.csv"), index=False)
-#     nodes.to_csv((graph_name+"_nodeslist_BOBDSG.csv"), index=False)
+def prepare_output_tables_Gephi_subgraph_all(df, subset_keys, graph_name):
+    sub_graph_edgelist = df.iloc[[count for count, value in enumerate(df["BehaviorOption"]) if any(True for x in subset_keys if x in value)]]
+    sub_graph_edgelist1 = sub_graph_edgelist[['BO_keys', 'BD_keys', 'sign_consist', 'counts_mention', 'counts_articles']].rename(columns={'BO_keys': 'Source', 'BD_keys': 'Target'})
+    sub_graph_edgelist2 = sub_graph_edgelist[['BD_keys', 'SG_keys', 'sign_consist', 'counts_mention', 'counts_articles']].rename(columns={'BD_keys': 'Source', 'SG_keys': 'Target'})
+    nodes = pd.concat([sub_graph_edgelist[['BO_keys', 'BehaviorOption']].rename(columns={'BO_keys':'Id', 'BehaviorOption': 'Label' }),
+                       sub_graph_edgelist[['BD_keys', 'BehaviorDeterminant']].rename(columns={'BD_keys':'Id', 'BehaviorDeterminant': 'Label' }),
+                       sub_graph_edgelist[['SG_keys', 'Studygroup']].rename(columns={'SG_keys':'Id', 'Studygroup': 'Label'})],
+                      ignore_index=True, axis=0)
+    l = len(sub_graph_edgelist.index)
+    nodes["Mode"] = 4
+    nodes["Mode"].iloc[:l] = 2
+    nodes["Mode"].iloc[l:(l*2)] = 3
+    nodes.loc[len(nodes.index)] = ['BO_class', graph_name , 1]
+    nodes = nodes.drop_duplicates()
+    superclass_df = pd.DataFrame({"Source": ['BO_class'] * len(set(sub_graph_edgelist['BO_keys'])),
+                                  "Target": list(dict.fromkeys(sub_graph_edgelist['BO_keys'])),
+                                  "sign_consist": [3]* len(set(sub_graph_edgelist['BO_keys'])),
+                                  "counts_mention": [100]* len(set(sub_graph_edgelist['BO_keys'])),
+                                  "counts_articles": [100]* len(set(sub_graph_edgelist['BO_keys']))})
+    sub_graph_edgelist = pd.concat([sub_graph_edgelist1, sub_graph_edgelist2, superclass_df], ignore_index=True, axis=0)
+    sub_graph_edgelist.to_csv(
+        (graph_name + "_edgelist_BOBDSG.csv"), index=False)
+    nodes.to_csv((graph_name+"_nodeslist_BOBDSG.csv"), index=False)
 #
 # # prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["walk"],
 # #                             graph_name = "walk")
 #
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["bike", "cycl"],
-#                             graph_name = "bike")
+prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["bike", "biking", "cycl"],
+                            graph_name = "bike")
+
+prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["public", "transit", "bus", "subway", "tram"],
+                            graph_name = "activeTravel")
+
 
 # prepare_output_tables_Gephi_subgraphBOBD(df = uniq_edgelist, subset_keys = ["walk"],
 #                             graph_name = "walk", Source_col = "BO_keys",
@@ -117,15 +121,15 @@ uniq_edgelist.to_csv("full_edgelist.csv", index=False)
 #                             Target_col = "BD_keys", Source_label = "BehaviorOption",
 #                             Target_label = "BehaviorDeterminant")
 #
-
-os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
-evidence_instances_full = pd.read_csv("unique_evidence_instances_clean2_harmonised_BO.csv")
-evidence_instances_full.replace({"-100": 'NaN'}, regex=False, inplace=True)
-evidence_instances_full.fillna("NaN")
-print(evidence_instances_full.head())
-
-for x in list(dict.fromkeys(evidence_instances_full["BehaviorOption"])):
-    print(x, len(evidence_instances_full[evidence_instances_full["BehaviorOption"]==x]))
+#
+# os.chdir(r"C:\Users\Tabea\Documents\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
+# evidence_instances_full = pd.read_csv("unique_evidence_instances_clean2_harmonised_BO.csv")
+# evidence_instances_full.replace({"-100": 'NaN'}, regex=False, inplace=True)
+# evidence_instances_full.fillna("NaN")
+# print(evidence_instances_full.head())
+#
+# for x in list(dict.fromkeys(evidence_instances_full["BehaviorOption"])):
+#     print(x, len(evidence_instances_full[evidence_instances_full["BehaviorOption"]==x]))
 
 #
 # evidence_instances_full['sign_consist'] = [1 if (evidence_instances_full['stat_significance'].iloc[i] == "significant")
