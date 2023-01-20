34e7d209d0325fb7432a84d3583f58ca32a468c3
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2023-01-20 10:53:58 +0100

    Update 16_Visualization.py

--- NLP Knowledge Extraction and Synthesis/16_Visualization.py
+++ NLP Knowledge Extraction and Synthesis/16_Visualization.py
@@ -3,37 +3,29 @@ import pandas as pd
 import numpy as np
 
 #####################################################################
+## This script uses the evidence instance dataframe to generate edgelists and nodelists for visualization of the knowledge graph.
+## This visualization can happen for example with the open source software Gephi
+################################################################################
+
 ### Functions
 def assign_keys_to_uniq_var(df, column, key_prefix):
+    ''' Takes a dataframe, column and key_prefix name
+    and generates keys for the unique values in the column and adds it to the dataframe'''
     uniq_val = set(uniq_edgelist[column])
     uniq_keys = [key_prefix + str(i) for i in range(1,(len(uniq_val)+1))]
     print("Nr of unique keys", len(uniq_keys))
     df[key_prefix+"keys"] = [uniq_keys[i] for x in df[column] for i,f in enumerate(uniq_val) if x == f]
     return df
-
-# Output dataframes can be read by gephi and when installing EventGraphLayout plugin transformed to bipartite graph visualization
-def prepare_output_tables_Gephi_subgraphBOBD(df, subset_keys, graph_name, Source_col, Target_col, Source_label, Target_label):
-    sub_graph_edgelist = df.iloc[[count for count, value in enumerate(df["BehaviorOption"]) if any(True for x in subset_keys if x in value)]]
-    sub_graph_edgelist.rename(columns={Source_col: 'Source', Target_col: 'Target'}, inplace=True)
-    nodes = pd.concat([sub_graph_edgelist[['Source', Source_label]].rename(columns={'Source':'Id', Source_label: 'Label' }),
-                       sub_graph_edgelist[['Target', Target_label]].rename(columns={'Target':'Id', Target_label: 'Label' })],
-                      ignore_index=True, axis=0)
-    nodes["Mode"] = 3
-    nodes["Mode"].iloc[:len(sub_graph_edgelist['Source'])] = 2
-    nodes.loc[len(nodes.index)] = ['BO_class', graph_name , 1]
-    nodes = nodes.drop_duplicates()
-    superclass_df = pd.DataFrame({"Source": ['BO_class'] * len(set(sub_graph_edgelist['Source'])),
-                                  "Target": list(dict.fromkeys(sub_graph_edgelist['Source'])),
-                                  "sign_consist": [3]* len(set(sub_graph_edgelist['Source'])),
-                                  "counts_mention": [100]* len(set(sub_graph_edgelist['Source'])),
-                                  "counts_articles": [100]* len(set(sub_graph_edgelist['Source']))})
-    sub_graph_edgelist = pd.concat([sub_graph_edgelist[['Source', 'Target', 'sign_consist', 'counts_mention', 'counts_articles']],
-                                    superclass_df], ignore_index=True, axis=0)
-    sub_graph_edgelist.to_csv(
-        (graph_name + "_edgelist.csv"), index=False)
     nodes.to_csv((graph_name+"_nodeslist.csv"), index=False)
-#
-def prepare_output_tables_Gephi_subgraph_all(df, subset_keys, graph_name, harmon_BOs = False):
+
+
+# Output dataframes can be read by gephi and when installing EventGraphLayout plugin transformed to tripartite graph visualization
+# This graph visualization function uses the harmonised Behavior Choice options only as an optional way to subselect the evidence instances
+# The verbatim original variable names are the labels in the nodes and edgelist outputs
+def prepare_output_tables_Gephi_subgraph_BOBDSG(df, subset_keys, graph_name, harmon_BOs = False):
+    ''' Generates a nodes- and an edgelist for the Behavior Choice Option (BO), Behavior Determinants (BD) and Studygroups (SG) for instances
+        where one of the strings in the subset keys is in the Behavior Choice option
+        or where it equals the value of a column (BehaviorOptionHarmon) with manually harmonised Behavior Choice options.'''
     if harmon_BOs:
         sub_graph_edgelist = df.iloc[np.where(df['BehaviorOptionHarmon'] == subset_keys[0])[0]]
     else:
@@ -62,7 +54,10 @@ def prepare_output_tables_Gephi_subgraph_all(df, subset_keys, graph_name, harmon
 
 
 # Output dataframes can be read by gephi and when installing EventGraphLayout plugin transformed to bipartite graph visualization
-def prepare_output_tables_Gephi(df, graph_name, Source_col, Target_col, Source_label, Target_label):
+# This graph visualization aggregates the evidence at the harmonised Behavior Choice Option level to simplify the output
+def prepare_output_tables_Gephi_harmonBOBD(df, graph_name, Source_col, Target_col, Source_label, Target_label):
+    ''' Generates a nodes- and an edgelist for the harmonised Behavior Choice Option (BO) and Behavior Determinants (BD)
+        for all evidence instances (no subgraph) but aggregated at the harmonised Behavior Choice Option level.'''
     subgraph = df[[Source_col, Target_col, Source_label,Target_label, 'sign_consist']]
     subgraph.drop_duplicates()
     subgraph[['counts_mention', 'counts_articles']] = 0
@@ -82,7 +77,7 @@ def prepare_output_tables_Gephi(df, graph_name, Source_col, Target_col, Source_l
     nodes.to_csv((graph_name+"_nodeslist.csv"), index=False)
 
 
-################################################################
+## Execution
 
 # Reading the evidence data
 os.chdir(r"D:\PhD EXPANSE\Written Paper\02- Behavioural Model paper")
@@ -124,63 +119,46 @@ print(uniq_edgelist["counts_mention"].value_counts())
 print("counts articles distribution:")
 print(uniq_edgelist["counts_articles"].value_counts())
 
-
+# Assigning keys to unique variables
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "BehaviorOption", "BO_")
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "BehaviorDeterminant", "BD_")
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "Studygroup", "SG_")
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "Moderator", "MO_")
 uniq_edgelist = assign_keys_to_uniq_var(uniq_edgelist, "BehaviorOptionHarmon", "BOharm_")
-
 uniq_edgelist.to_csv("full_edgelist.csv", index=False)
 
+# generating Output tables for different modes and graph visualizations
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["biking"],
+                            graph_name = "bike", harmon_BOs=True)
 
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["biking"],
-#                             graph_name = "bike", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["walking"],
-#                             graph_name = "walking", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["active school travel"],
-#                             graph_name = "activeschooltravel", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["driving"],
-#                             graph_name = "driving", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["physical activity"],
-#                             graph_name = "pysicalactivity", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["active travel"],
-#                             graph_name = "activetravel", harmon_BOs=True)
-#
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["public transport use"],
-#                             graph_name = "transit", harmon_BOs=True)
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["walking"],
+                            graph_name = "walking", harmon_BOs=True)
 
-prepare_output_tables_Gephi(df = uniq_edgelist, graph_name = "harmonise_BO_final", Source_col = "BOharm_keys",
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["active school travel"],
+                            graph_name = "activeschooltravel", harmon_BOs=True)
+
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["driving"],
+                            graph_name = "driving", harmon_BOs=True)
+
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["physical activity"],
+                            graph_name = "pysicalactivity", harmon_BOs=True)
+
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["active travel"],
+                            graph_name = "activetravel", harmon_BOs=True)
+
+prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["public transport use"],
+                            graph_name = "transit", harmon_BOs=True)
+
+prepare_output_tables_Gephi_harmonBOBD(df = uniq_edgelist, graph_name = "harmonise_BO_final", Source_col = "BOharm_keys",
                             Target_col = "BD_keys", Source_label = "BehaviorOptionHarmon",
                             Target_label = "BehaviorDeterminant")
 
 
-######## Former versions before manual harmonisation of Behavior Options
 
-#
-# # prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["walk"],
-# #                             graph_name = "walk")
-#
-#prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["bike", "biking", "cycl"],
- #                           graph_name = "bike")
+## use of script without manually harmonised Behavior Choice Options
 
-# prepare_output_tables_Gephi_subgraph_all(df = uniq_edgelist, subset_keys = ["public", "transit", "bus", "subway", "tram"],
-#                          graph_name = "activeTravel")
+# prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["bike", "biking", "cycl"],
+#                           graph_name = "bike", harmon_BOs = False)
 
-
-# prepare_output_tables_Gephi_subgraphBOBD(df = uniq_edgelist, subset_keys = ["walk"],
-#                             graph_name = "walk", Source_col = "BO_keys",
-#                             Target_col = "BD_keys", Source_label = "BehaviorOption",
-#                             Target_label = "BehaviorDeterminant")
-#
-# prepare_output_tables_Gephi_subgraphBOBD(df = uniq_edgelist, subset_keys = ["bike", "cycl"],
-#                             graph_name = "bike", Source_col = "BO_keys",
-#                             Target_col = "BD_keys", Source_label = "BehaviorOption",
-#                             Target_label = "BehaviorDeterminant")
-#
-
+# prepare_output_tables_Gephi_subgraph_BOBDSG(df = uniq_edgelist, subset_keys = ["public", "transit", "bus", "subway", "tram"],
+#                          graph_name = "publicTransport", , harmon_BOs = False)
