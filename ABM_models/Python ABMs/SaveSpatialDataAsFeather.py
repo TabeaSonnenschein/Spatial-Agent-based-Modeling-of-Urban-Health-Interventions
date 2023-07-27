1eadfdf2b5d8221e68b3b06e5afd0c986192b526
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2023-07-27 18:35:57 +0200

    Update SaveSpatialDataAsFeather.py

--- ABM_models/Python ABMs/SaveSpatialDataAsFeather.py
+++ ABM_models/Python ABMs/SaveSpatialDataAsFeather.py
@@ -18,6 +18,7 @@ shape_file_Nightlife = gpd.read_file(path_data +"Built Environment/Facilities/Am
 shape_file_Profess = gpd.read_file(path_data +"Built Environment/Facilities/Amsterdam_Foursquarevenues_Profess_other_RDNew.shp")
 spatial_extent = gpd.read_file(path_data+"SpatialExtent/Amsterdam Diemen Oude Amstel Extent.shp")
 shape_EnvBehavDeterminants = gpd.read_file(path_data+"Built Environment/Transport Infrastructure/ModalChoice_determ_200.shp")
+shape_AirPollPreds = gpd.read_file(path_data+"Air Pollution Determinants/AirPollDeterm_grid50.shp")
 
 shape_file_buildings.to_feather(path_data+"FeatherDataABM/Buildings.feather")
 shape_file_streets.to_feather(path_data+"FeatherDataABM/Streets.feather" )
@@ -34,3 +35,4 @@ shape_file_Nightlife.to_feather(path_data+"FeatherDataABM/Nightlife.feather")
 shape_file_Profess.to_feather(path_data+"FeatherDataABM/Profess.feather" )
 spatial_extent.to_feather(path_data+"FeatherDataABM/SpatialExtent.feather")
 shape_EnvBehavDeterminants.to_feather(path_data+"FeatherDataABM/EnvBehavDeterminants.feather")
+shape_AirPollPreds.to_feather(path_data+"FeatherDataABM/AirPollgrid50m.feather")
