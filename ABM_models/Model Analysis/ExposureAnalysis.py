b1687befaacdc03c3bc8ac9b8180734b7378047f
TabeaSonnenschein <62036067+TabeaSonnenschein@users.noreply.github.com>
2023-08-24 16:07:04 +0200

    x

--- ABM_models/Model Analysis/ExposureAnalysis.py
+++ ABM_models/Model Analysis/ExposureAnalysis.py
@@ -5,54 +5,96 @@ import seaborn as sns
 ####################################################
 
 
-def ScatterOverTimeColCategory(outcomvar, colcategory, showplots):
+def ScatterOverTimeColCategory(outcomvar, colcategory, showplots, modelrun, ylabel = None, collabel = None):
+    if ylabel is None:
+        ylabel = outcomvar
+    if collabel is None:
+        collabel = colcategory
     sns.set(style="whitegrid")
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x="hour", y=outcomvar, hue=colcategory, data=exposure_df, alpha=0.4)
     plt.xlabel("Hour")
-    plt.ylabel(outcomvar)
-    plt.title(f"Distribution of {outcomvar} Over Time by {colcategory}")
-    plt.legend(title="Sex")
-    plt.savefig(f'scatterplot_{outcomvar}_by_{colcategory}.pdf', dpi = 300)
+    plt.ylabel(ylabel)
+    plt.title(f"Distribution of {outcomvar} Over Time by {collabel}")
+    plt.legend(title=collabel)
+    plt.savefig(f'{modelrun}_scatterplot_{outcomvar}_by_{colcategory}.pdf', dpi = 300)
     if showplots:
         plt.show()
     plt.close()
+    
 
-def ScatterOverTimeColContinous(outcomvar, colvar, showplots):
+def ScatterOverTimeColContinous(outcomvar, colvar, showplots, modelrun, ylabel = None, collabel = None):
+    if ylabel is None:
+        ylabel = outcomvar
+    if collabel is None:
+        collabel = colvar
     sns.set(style="whitegrid")
     color_map = sns.color_palette("RdYlGn_r", as_cmap=True)
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x="hour", y=outcomvar, hue=colvar, data=exposure_df, alpha=0.4, palette=color_map)
     plt.xlabel("Hour")
-    plt.ylabel(outcomvar)
-    plt.title(f"Distribution of {outcomvar} Over Time by {colvar}")
-    plt.legend(title="Sex")
-    plt.savefig(f'scatterplot_{outcomvar}_by_{colvar}.pdf', dpi = 300)
+    plt.ylabel(ylabel)
+    plt.title(f"Distribution of {outcomvar} Over Time by {collabel}")
+    plt.legend(title=collabel)
+    plt.savefig(f'{modelrun}_scatterplot_{outcomvar}_by_{colvar}.pdf', dpi = 300)
+    if showplots:
+        plt.show()
+    plt.close()
+    
+
+def LineOverTimeColCategory(outcomvar, colcategory, showplots, modelrun, ylabel = None, collabel = None):
+    if ylabel is None:
+        ylabel = outcomvar
+    if collabel is None:
+        collabel = colcategory
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(x="hour", y=outcomvar, hue=colcategory, data=exposure_df, alpha=0.4)
+    plt.xlabel("Hour")
+    plt.ylabel(ylabel)
+    plt.title(f"Distribution of {outcomvar} Over Time by {collabel}")
+    plt.legend(title=collabel)
+    plt.savefig(f'{modelrun}_lineplot_{outcomvar}_by_{colcategory}.pdf', dpi = 300)
+    if showplots:
+        plt.show()
+    plt.close()
+    
+
+def LineOverTimeColContinous(outcomvar, colvar, showplots, modelrun, ylabel = None, collabel = None):
+    if ylabel is None:
+        ylabel = outcomvar
+    if collabel is None:
+        collabel = colvar
+    sns.set(style="whitegrid")
+    color_map = sns.color_palette("RdYlGn_r", as_cmap=True)
+    plt.figure(figsize=(10, 6))
+    sns.lineplot(x="hour", y=outcomvar, hue=colvar, data=exposure_df, alpha=0.4, palette=color_map)
+    plt.xlabel("Hour")
+    plt.ylabel(ylabel)
+    plt.title(f"Distribution of {outcomvar} Over Time by {collabel}")
+    plt.legend(title=collabel)
+    plt.savefig(f'{modelrun}_lineplot_{outcomvar}_by_{colvar}.pdf', dpi = 300)
     if showplots:
         plt.show()
     plt.close()
 
-
-
-
-
-
-
-
-
-
 ######################################################
 nb_agents = 43500
 # path_data = "C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/"
-path_data ="/Users/tsonnens/Documents/Exposure Results"
+# path_data ="/Users/tsonnens/Documents/Exposure Results"
+path_data ="/Users/tsonnens/Documents/Exposure Intervention"
+
 # os.chdir(path_data + 'ModelRuns/AgentExposure/')
 os.chdir(path_data)
 
-modelrun = "StatusQuo"
+# modelrun = "StatusQuo"
+modelrun = "SpeedInterv"
+
 
 print("Reading the data")
 # read exposure data
-exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_hourAsRowsMerged.csv")
+exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_{modelrun}_hourAsRowsMerged.csv")
+
 
 print("Analyzing the data")
 print(exposure_df.head())
@@ -64,30 +106,129 @@ for column in exposure_df.select_dtypes(include=['object']):
 
 
 # Renaming some columns for elegance
-exposure_df = exposure_df.rename(columns={"incomeclass_int": "income"})
+exposure_df = exposure_df.rename(columns={"incomeclass_int": "income", "migrationbackground": "migr_bck"})
 
 print("Plotting the data")
-figuresToPlot = ["sex_scatterNO2","sex_scatterMET", "income_scatterNO2", "income_scatterMET"]
+# figuresToPlot = ["sex_scatterNO2","sex_scatterMET", "income_scatterNO2", "income_scatterMET", "migr_bck_scatterNO2", "migr_bcksex_scatterMET",
+                #  "sex_lineNO2", "sex_lineMET", "income_lineNO2", "income_lineMET", "migr_bck_lineNO2", "migr_bcksex_lineMET"]
+figuresToPlot = []
 showplots = False
 
+
+print("Plotting Scatterplots")
+
 if "sex_scatterNO2" in figuresToPlot:
-    ScatterOverTimeColCategory(outcomvar="NO2", colcategory="sex", showplots=showplots)
+    ScatterOverTimeColCategory(outcomvar="NO2", colcategory="sex", showplots=showplots, modelrun=modelrun, ylabel="NO2 (µg/m3)")
 
 if "sex_scatterMET" in figuresToPlot:
-    ScatterOverTimeColCategory(outcomvar="MET", colcategory="sex", showplots=showplots)
+    ScatterOverTimeColCategory(outcomvar="MET", colcategory="sex", showplots=showplots, modelrun=modelrun, ylabel="Metabolic Equivalent of Task (MET)")
 
 if "income_scatterNO2" in figuresToPlot:
-    ScatterOverTimeColContinous(outcomvar="NO2", colvar="income", showplots=showplots)
+    ScatterOverTimeColContinous(outcomvar="NO2", colvar="income", showplots=showplots, modelrun=modelrun, collabel="Income Percentile", ylabel="NO2 (µg/m3)")
 
 if "income_scatterMET" in figuresToPlot:
-    ScatterOverTimeColContinous(outcomvar="MET", colvar="income", showplots=showplots)
+    ScatterOverTimeColContinous(outcomvar="MET", colvar="income", showplots=showplots, modelrun=modelrun, collabel="Income Percentile", ylabel="Metabolic Equivalent of Task (MET)")
+
+if "migr_bck_scatterNO2" in figuresToPlot:
+    ScatterOverTimeColCategory(outcomvar="NO2", colcategory="migr_bck", showplots=showplots, modelrun=modelrun, ylabel="NO2 (µg/m3)", collabel="Migration Background")
+
+if "migr_bcksex_scatterMET" in figuresToPlot:
+    ScatterOverTimeColCategory(outcomvar="MET", colcategory="migr_bck", showplots=showplots, modelrun=modelrun, ylabel="Metabolic Equivalent of Task (MET)", collabel="Migration Background")
+
+print("Plotting Lineplots")
+    
+if "sex_lineNO2" in figuresToPlot:
+    LineOverTimeColCategory(outcomvar="NO2", colcategory="sex", showplots=showplots, modelrun=modelrun, ylabel="NO2 (µg/m3)")
+
+if "sex_lineMET" in figuresToPlot:
+    LineOverTimeColCategory(outcomvar="MET", colcategory="sex", showplots=showplots, modelrun=modelrun, ylabel="NO2 (µg/m3)")
+    
+if "income_lineNO2" in figuresToPlot:
+    LineOverTimeColContinous(outcomvar="NO2", colvar="income", showplots=showplots, modelrun=modelrun, collabel="Income Percentile", ylabel="NO2 (µg/m3)")
+
+if "income_lineMET" in figuresToPlot:
+    LineOverTimeColContinous(outcomvar="MET", colvar="income", showplots=showplots, modelrun=modelrun, collabel="Income Percentile", ylabel="Metabolic Equivalent of Task (MET)")
+
+if "migr_bck_lineNO2" in figuresToPlot:
+    LineOverTimeColCategory(outcomvar="NO2", colcategory="migr_bck", showplots=showplots, modelrun=modelrun, ylabel="NO2 (µg/m3)", collabel="Migration Background")
+
+if "migr_bcksex_lineMET" in figuresToPlot:
+    LineOverTimeColCategory(outcomvar="MET", colcategory="migr_bck", showplots=showplots, modelrun=modelrun, ylabel="Metabolic Equivalent of Task (MET)", collabel="Migration Background")
+
+
+
+
+#########################
+### Comparative Plots ###
+#########################
+# path_data = "C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/"
+path_data ="/Users/tsonnens/Documents/Exposure Results"
+# path_data ="/Users/tsonnens/Documents/Exposure Intervention"
+
+# os.chdir(path_data + 'ModelRuns/AgentExposure/')
+os.chdir(path_data)
+
+modelrun = "StatusQuo"
+# modelrun = "SpeedInterv"
+
+
+print("Reading the data")
+# read exposure data
+exposure_df_statusq = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_hourAsRowsMerged.csv")
+
+exposure_comp = pd.merge(exposure_df, exposure_df_statusq, on=["agent_ID", "hour"], how="left", suffixes=("_interv", "_statusq"))
+    
+# Set up Seaborn style
+sns.set(style="whitegrid")
+
+# Create the line plot for NO2 before intervention
+plt.figure(figsize=(10, 6))
+sns.lineplot(x="hour", y="NO2_statusq", data=exposure_comp, label="Before Intervention")
+
+# Add the line plot for NO2 after intervention
+sns.lineplot(x="hour", y="NO2_interv", data=exposure_comp, label="After Intervention")
+
+# Customize plot
+plt.xlabel("Hour")
+plt.ylabel("NO2")
+plt.title("Comparison of NO2 Before and After Intervention Over Time")
+plt.legend()
+
+plt.savefig(f'{modelrun}_lineplot_NO2_comparedToStatusQuo.pdf', dpi = 300)
+
+# Display the plot
+plt.show()
+
+
+
+    
+# Set up Seaborn style
+sns.set(style="whitegrid")
+
+# Create the line plot for NO2 before intervention
+plt.figure(figsize=(10, 6))
+sns.lineplot(x="hour", y="MET_statusq", data=exposure_comp, label="Before Intervention")
+
+# Add the line plot for NO2 after intervention
+sns.lineplot(x="hour", y="MET_interv", data=exposure_comp, label="After Intervention")
+
+# Customize plot
+plt.xlabel("Hour")
+plt.ylabel("MET")
+plt.title("Comparison of MET Before and After Intervention Over Time")
+plt.legend()
+
+plt.savefig(f'{modelrun}_lineplot_MET_comparedToStatusQuo.pdf', dpi = 300)
+
+# Display the plot
+plt.show()
 
 
 
 # plot of exposure distribution per hour per different sociodemographic groups
 # # sex
-sexdf = exposure_df[["sex", "hour", "NO2", "MET"]].groupby(["sex", "hour"], as_index=False).mean()
-print(sexdf.head())    
+# sexdf = exposure_df[["sex", "hour", "NO2", "MET"]].groupby(["sex", "hour"], as_index=False).mean()
+# print(sexdf.head())    
 
 # plot the distribution of NO2 per sex per hour
 
--- ABM_models/Model Analysis/PreparingExpData.py
+++ ABM_models/Model Analysis/PreparingExpData.py
@@ -3,8 +3,8 @@ import os
 ####################################################
 nb_agents = 43500
 # path_data = "C:/Users/Tabea/Documents/PhD EXPANSE/Data/Amsterdam/"
-path_data ="/Users/tsonnens/Documents/Exposure Results"
-
+path_data ="/Users/tsonnens/Documents/Exposure Intervention"
+modelrun = "SpeedInterv"
 # read exposure data
 # os.chdir(path_data + 'ModelRuns/AgentExposure/')
 os.chdir(path_data)
@@ -13,7 +13,7 @@ print(listOfFiles)
 
 # read the exposure data for each hour and merge them into one
 for hour in range(24):
-    exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_H{hour}.csv")
+    exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_H{hour}_{modelrun}.csv")
     exposure_df = exposure_df.rename(columns={"NO2": f"NO2_{hour}", "MET": f"MET_{hour}"})
     if hour == 0:
         exposure_df_all = exposure_df
@@ -21,11 +21,11 @@ for hour in range(24):
         exposure_df_all = pd.merge(exposure_df, exposure_df_all, on="agent", how="left")
 
 exposure_df_all = exposure_df_all.rename(columns={"agent": "agent_ID"})
-exposure_df_all.to_csv(f"AgentExposure_A{nb_agents}_M1_hourAsColumns.csv", index=False)
+exposure_df_all.to_csv(f"AgentExposure_A{nb_agents}_M1_{modelrun}_hourAsColumns.csv", index=False)
 
 # read the exposure data for each hour and merge them into one whereby the hour is a row 
 for hour in range(24):
-    exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_H{hour}.csv")
+    exposure_df = pd.read_csv(f"AgentExposure_A{nb_agents}_M1_H{hour}_{modelrun}.csv")
     exposure_df["hour"] = hour
     if hour == 0:
         exposure_df_all = exposure_df
@@ -33,7 +33,7 @@ for hour in range(24):
         exposure_df_all = pd.concat( [exposure_df_all,exposure_df], axis=0)
         
 exposure_df_all = exposure_df_all.rename(columns={"agent": "agent_ID"})
-exposure_df_all.to_csv(f"AgentExposure_A{nb_agents}_M1_hourAsRows.csv", index=False)
+exposure_df_all.to_csv(f"AgentExposure_A{nb_agents}_M1_{modelrun}_hourAsRows.csv", index=False)
 
 
 print("Reading the  Agent data")
@@ -43,5 +43,5 @@ agent_df = pd.read_csv("Amsterdam_population_subset.csv")
 print("Merging the data")
 # merge exposure data with agent_df
 exposure_df = pd.merge(agent_df, exposure_df_all, on="agent_ID", how="right")
-exposure_df.to_csv(f"AgentExposure_A{nb_agents}_M1_hourAsRowsMerged.csv", index=False)
+exposure_df.to_csv(f"AgentExposure_A{nb_agents}_M1_{modelrun}_hourAsRowsMerged.csv", index=False)
 
