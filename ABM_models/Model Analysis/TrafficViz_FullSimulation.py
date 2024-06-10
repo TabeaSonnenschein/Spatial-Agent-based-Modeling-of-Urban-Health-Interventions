b8549f72e27d80db2c5187b8d8557b1ff1d26b68
TabeaSonnenschein <tabea.sonnenschein@gmail.com>
2024-06-10 12:01:26 +0200

    Update TrafficViz_FullSimulation.py

--- ABM_models/Model Analysis/TrafficViz_FullSimulation.py
+++ ABM_models/Model Analysis/TrafficViz_FullSimulation.py
@@ -25,13 +25,13 @@ scenario = "StatusQuo"
 # scenario ="PedStrWidthCenter"
 # scenario = "PedStrLenCenter"
 # scenario = "PedStrLenOutskirt"
-# scenario = "PrkPriceInterv"
+scenario = "PrkPriceInterv"
 
 cellsize = 50
 
 experimentoverview = pd.read_csv("D:/PhD EXPANSE/Data/Amsterdam/ABMRessources/ABMData/ExperimentOverview.csv")
 modelruns = experimentoverview.loc[experimentoverview["Experiment"] == scenario, "Model Run"].values
-modelruns = [481658] 
+modelruns = [381609] 
 bestStatusQuo = 481658
 
 os.chdir(path_data)
@@ -97,33 +97,33 @@ distance_meters = points[0].distance(points[1])
 print(distance_meters)
 
 if "singleIntervention" in viztype:
-    # print("singleIntervention")
+    print("singleIntervention")
         
-    # AirPollGrid_x["mean_prTraff"] = AirPollGrid_x[totTraffcols].mean(axis=1)
-    # AirPollGrid_x["mean_TrCount"] = AirPollGrid_x[totTraffCcols].mean(axis=1)
+    AirPollGrid_x["mean_prTraff"] = AirPollGrid_x[totTraffcols].mean(axis=1)
+    AirPollGrid_x["mean_TrCount"] = AirPollGrid_x[totTraffCcols].mean(axis=1)
 
-    # for month in [1, 4,7,10]:
-    #     monthlycols = [col for col in AirPollGrid_x.columns if f"prTraff_M{month}" in col]
-    #     AirPollGrid_x[f"mean_prTraff_M{month}"] = AirPollGrid_x[monthlycols].mean(axis=1)
+    for month in [1, 4,7,10]:
+        monthlycols = [col for col in AirPollGrid_x.columns if f"prTraff_M{month}" in col]
+        AirPollGrid_x[f"mean_prTraff_M{month}"] = AirPollGrid_x[monthlycols].mean(axis=1)
 
-    # for hour in range(24):
-    #     #hourly cols if column name ends with f"H{hour}"
-    #     hourlycols = [col for col in AirPollGrid_x.columns if col.endswith(f"H{hour}")]
-    #     AirPollGrid_x[f"mean_prTraff_H{hour}"] = AirPollGrid_x[hourlycols].mean(axis=1)
+    for hour in range(24):
+        #hourly cols if column name ends with f"H{hour}"
+        hourlycols = [col for col in AirPollGrid_x.columns if col.endswith(f"H{hour}")]
+        AirPollGrid_x[f"mean_prTraff_H{hour}"] = AirPollGrid_x[hourlycols].mean(axis=1)
     
-    # for weekday in days_order: 
-    #     weekdaycolentries = [f"prTraff_M{date[0]}_D{date[1]}" for date in datedf.loc[datedf["weekday"] == weekday, ["month", "day"]].values]
-    #     weekdaycols = [col for entry in weekdaycolentries for col in AirPollGrid_x.columns  if entry in col]
-    #     AirPollGrid_x[f"mean_prTraff_{weekday}"] = AirPollGrid_x[weekdaycols].mean(axis=1)
+    for weekday in days_order: 
+        weekdaycolentries = [f"prTraff_M{date[0]}_D{date[1]}" for date in datedf.loc[datedf["weekday"] == weekday, ["month", "day"]].values]
+        weekdaycols = [col for entry in weekdaycolentries for col in AirPollGrid_x.columns  if entry in col]
+        AirPollGrid_x[f"mean_prTraff_{weekday}"] = AirPollGrid_x[weekdaycols].mean(axis=1)
     
     
-    # colnames = ["mean_prTraff", "mean_TrCount"]+[f"mean_prTraff_M{month}" for month in [1,4,7,10]] + [f"mean_prTraff_H{hour}" for hour in range(24)] + [f"mean_prTraff_{weekday}" for weekday in days_order] + ["prTraff_M1_D1_H8", "TrCount_M1_D1_H8", "prTraff_M1_D2_H2", "TrCount_M1_D2_H2"]
-    # labels = ["Mean annual Traffic Volumn", "Mean annual Traffic Count"] + [f"Mean Traffic Volume in {monthsnames[month-1]}" for month in [1, 4,7,10]] + [f"Mean Traffic Volume at Hour {hour}" for hour in range(24)] + [f"Mean Traffic Volume on {weekday}s" for weekday in days_order] + ["Traffic Volume at: 01.01 at 8am", "Traffic Count at: 01.01 at 8am", "Traffic Volume at: 02.01 at 2am", "Traffic Count at: 02.01 at 2am"]
+    colnames = ["mean_prTraff", "mean_TrCount"]+[f"mean_prTraff_M{month}" for month in [1,4,7,10]] + [f"mean_prTraff_H{hour}" for hour in range(24)] + [f"mean_prTraff_{weekday}" for weekday in days_order] + ["prTraff_M1_D1_H8", "TrCount_M1_D1_H8", "prTraff_M1_D2_H2", "TrCount_M1_D2_H2"]
+    labels = ["Mean annual Traffic Volumn", "Mean annual Traffic Count"] + [f"Mean Traffic Volume in {monthsnames[month-1]}" for month in [1, 4,7,10]] + [f"Mean Traffic Volume at Hour {hour}" for hour in range(24)] + [f"Mean Traffic Volume on {weekday}s" for weekday in days_order] + ["Traffic Volume at: 01.01 at 8am", "Traffic Count at: 01.01 at 8am", "Traffic Volume at: 02.01 at 2am", "Traffic Count at: 02.01 at 2am"]
     os.chdir(destination)
-    # MapSpatialDataFixedColorMapSetofVariables(variables = colnames, rasterdf = AirPollGrid_x, jointlabel = "", 
-    #                                             specificlabel = labels, vmin=0, vmax=400,distance_meters= distance_meters, 
-    #                                             cmap="turbo", suffix=f"{scenario}_{modelrun}")
-    # AirPollGrid_x[["int_id"]+colnames].to_csv(destination+f"/TraffMeans_{scenario}_{modelrun}.csv", index=False)
+    MapSpatialDataFixedColorMapSetofVariables(variables = colnames, rasterdf = AirPollGrid_x, jointlabel = "", 
+                                                specificlabel = labels, vmin=0, vmax=400,distance_meters= distance_meters, 
+                                                cmap="turbo", suffix=f"{scenario}_{modelrun}")
+    AirPollGrid_x[["int_id"]+colnames].to_csv(destination+f"/TraffMeans_{scenario}_{modelrun}.csv", index=False)
     AirPollGrid_x.plot("TrCount_M1_D1_H8", antialiased=False, legend = True)
     plt.title(f"Traffic Count at: 01.01 at 8am")
     plt.savefig(f'AirPollGrid_TraffC_M1_D1_H8_{scenario}_{modelrun}.png')
@@ -143,35 +143,35 @@ if "singleIntervention" in viztype:
     
 if "statusquocomparison" in viztype:
     print("statusquocomparison")
+    os.chdir(destination)
 
-    NO2dat_interv = pd.read_csv(f"NO2/{modelrun}/AirPollGrid_NO2_pred{nb_agents}_{modelrun}.csv")
-    NO2dat_statquo = pd.read_csv(f"NO2/StatusQuo/AirPollGrid_NO2_pred{nb_agents}_StatusQuo.csv")
+    Traffdat_interv = pd.read_csv(f"TraffMeans_{scenario}_{modelrun}.csv")
+    Traffdat_statquo = pd.read_csv(f"/StatusQuo/{nb_agents}Agents/Traffic/TraffViz/TraffMeans_StatusQuo_{bestStatusQuo}.csv")
 
     # calculate hourly difference
-    NO2diff =  NO2dat_interv[NO2vars]- NO2dat_statquo[NO2vars]
-    NO2diff_cols = [var + "_diff" for var in NO2vars]
-    NO2diff.columns = NO2diff_cols
+    colnames = ["mean_prTraff", "mean_TrCount"]+[f"mean_prTraff_M{month}" for month in [1,4,7,10]] + [f"mean_prTraff_H{hour}" for hour in range(24)] + [f"mean_prTraff_{weekday}" for weekday in days_order] + ["prTraff_M1_D1_H8", "TrCount_M1_D1_H8", "prTraff_M1_D2_H2", "TrCount_M1_D2_H2"]
 
-    NO2diff["mean_NO2_diff"] = NO2diff.mean(axis=1)
-    meansperhour = list(NO2diff.mean(axis=0))
-    print("means per hour", meansperhour)
-    NO2diff["int_id"] = NO2dat_interv["int_id"]
-    NO2diff.to_csv(f"NO2/AirPollGrid_NO2_diff{nb_agents}_{modelrun}.csv", index=False)
-    print(NO2diff.describe())
+    Traffdiff =  Traffdat_interv[colnames]- Traffdat_statquo[colnames]
+    Traffdiff_cols = [var + "_diff" for var in colnames]
+    Traffdiff.columns = Traffdiff_cols
 
-    AirPollGrid = AirPollGrid.merge(NO2diff, on="int_id", how="left")
+    Traffdiff["mean_NO2_diff"] = Traffdiff.mean(axis=1)
+    meanspercol = list(Traffdiff.mean(axis=0))
+    print("means per column", meanspercol)
+    Traffdiff["int_id"] = Traffdat_interv["int_id"]
+    Traffdiff.to_csv(f"TraffDiff_{scenario}_StatQuo_{modelrun}.csv", index=False)
+    print(Traffdiff.describe())
 
-    AirPollGrid.plot("mean_NO2_diff", antialiased=False, legend = True)
-    plt.title(f"Mean NO2 Difference between {modelrun} and Status Quo")
-    plt.savefig(f'NO2/AirPollGrid_NO2_MeanDiff{nb_agents}_{modelrun}.png')
+    AirPollGrid_y = AirPollGrid.merge(Traffdiff, on="int_id", how="left")
+
+    AirPollGrid_y.plot("mean_prTraff_diff", antialiased=False, legend = True)
+    plt.title(f"Mean Traffic Volume Difference between {modelrun} and Status Quo")
+    plt.savefig(f'Traff_MeanDiff{nb_agents}_{modelrun}.png')
     plt.close()
 
-    AirPollGrid.plot(NO2diff_cols[meansperhour.index(min(meansperhour))], antialiased=False, legend = True)
-    plt.title(f"Max NO2 Difference between {modelrun} and Status Quo: Hour {meansperhour.index(min(meansperhour))}")
-    plt.savefig(f'NO2/AirPollGrid_NO2_MaxDiff{nb_agents}_{modelrun}.png')
-    plt.close()
+    labels = ["Mean annual Traffic Volumn", "Mean annual Traffic Count"] + [f"Mean Traffic Volume in {monthsnames[month-1]}" for month in [1, 4,7,10]] + [f"Mean Traffic Volume at Hour {hour}" for hour in range(24)] + [f"Mean Traffic Volume on {weekday}s" for weekday in days_order] + ["Traffic Volume at: 01.01 at 8am", "Traffic Count at: 01.01 at 8am", "Traffic Volume at: 02.01 at 2am", "Traffic Count at: 02.01 at 2am"]
+    labelsdiff = [f"{label} Difference" for label in labels]
 
-    AirPollGrid.plot(NO2diff_cols[meansperhour.index(max(meansperhour))], antialiased=False, legend = True)
-    plt.title(f"Max NO2 Difference between {modelrun} and Status Quo: Hour {meansperhour.index(min(meansperhour))}")
-    plt.savefig(f'NO2/AirPollGrid_NO2_MinDiff{nb_agents}_{modelrun}.png')
-    plt.close()
+    MapSpatialDataFixedColorMapSetofVariables(variables = Traffdiff_cols, rasterdf = AirPollGrid_y, jointlabel = "", 
+                                                specificlabel = labels, vmin=0, vmax=100,distance_meters= distance_meters, 
+                                                cmap="turbo", suffix=f"{scenario}_{modelrun}")
